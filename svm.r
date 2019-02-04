bc27b1cdac7a5431d1b887cee98abeaa44c71345
Marta Verona <marveral333@gmail.com>
2019-02-04 19:51:05 +0100

    update svm

--- svm.r
+++ svm.r
@@ -1,42 +1,63 @@
 require('e1071')
 set.seed(1234)
 
+predictAndSave <- function(train, test,filename,kernel="radial"){
+  model <- svm(C~.,data = train,kernel=kernel)
+  pred <- predict(model,train)
+  print(pred)
+  #pred <- round(pred)
+  print(mean(pred==train$C))
+  pred <- predict(model,test)
+  #write.csv(data.frame('Id'=c(1:length(pred)),'Prediction'=round(pred)), file = filename, row.names=FALSE)
+  write.csv(data.frame('Id'=c(1:length(pred)),'Prediction'=pred), file = filename, row.names=FALSE)
+  #return(pred)
+}
+
+
 data <- read.csv("/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/train.csv", header=TRUE, na.strings="?")
 test.data <- read.csv("/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/test.csv", header=TRUE, na.strings="?")
 
 # CENTER AND SCALE DATA
-require(caret)
-n <- length(data)
-valoresPreprocesados <- caret::preProcess(data[1:n-1],method=c("center" ,"scale") )
-data.scaled <- predict(valoresPreprocesados,data[1:n-1])
-data.scaled <- cbind(data.scaled,C=data$C)
-
-
-predictAndSave <- function(train, test,filename,kernel="radial"){
-  model <- svm(C~.,data = train,kernel=kernel)
-  pred <- predict(model,train)
-  pred <- round(pred)
-  print(mean(pred==train$C))
-  pred <- predict(model,test)
-  write.csv(data.frame('Id'=c(1:length(pred)),'Prediction'=round(pred)), file = filename, row.names=FALSE)
-  pred
-}
+new_data = preProcessData(data,test.data)
+scaled.train = new_data[[1]]
+scaled.test = new_data[[2]]
 
 # FILL MISSING VALUES
-train.data <- computeMissingValues(data.scaled,type='knn',1)
+scaled.train.knn = computeMissingValues(scaled.train, type = "knn")
+predictAndSave(scaled.train,scaled.test,'/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/knn-scaled.csv')
+
+scaled.train = computeMissingValues(scaled.train, type = "rf")
+write.csv(data.frame('Id'=c(1:length(pred)),'Prediction'=round(pred)), file = filename, row.names=FALSE)
+predictAndSave(scaled.train,scaled.test,'/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/rf-missing.csv')
+
 
 # OUTLIERS
-k <- 1
-a <- computeOutliers(train.data, type='knn', k = k)
-predictAndSave(a,test.data,paste(paste('./outliers-knn-',k),'.csv'))
+scaled.train.outliers.knn <- computeOutliers(scaled.train, type='knn')
+predictAndSave(scaled.train.outliers.knn,scaled.test,'/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/knn-scaled+outliers.csv')
 
+scaled.train.outliers.mean <- computeOutliers(scaled.train.knn, type='mean')
+predictAndSave(scaled.train.outliers.mean,scaled.test,'/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/knn-scaled-mean-outliers.csv')
+
+
+# UNBALANCE
+round(prop.table(table(data$C)) * 100, digits = 1)
+balance.types = c('ubOver', 'ubUnder', 'ubSMOTE')
+i <- 1
+while( i < length(balance.types)){
+  scaled.train.outliers.mean.balanced <- solveUnbalance(scaled.train.outliers.mean,type=balance.types[i])
+  predictAndSave(scaled.train.outliers.mean.balanced, scaled.test,paste(balance.types[i],'-balance.csv'))
+}
+
+
+# REMOVE COLUMNS WITH LOWER CORRELATION WITH OUTPUT
+scaled.train.remove.low.cor <- removeLowCorrelationAttributes(scaled.train)
+predictAndSave(scaled.train.remove.low.cor,scaled.test,'/Users/marta/Dropbox/MASTER/Minería de datos, preprocesamiento y clasificación/Trabajo/knn-scaled-mean-outliers-remove-low-cor.csv')
 
 
 # KERNELS
-kernels <- c("linear","polynomial","radial basis","sigmoid")
+kernels <- c("radial","polynomial")
 i<-1
 while( i< length(kernels)){
-  predictAndSave(train, test,paste(kernels[i],'.csv'),kernel=kernels[i])
+  predictAndSave(scaled.train.outliers.mean, scaled.test,paste(kernels[i],'.csv'),kernel=kernels[i])
   i<- i+1
 }
-predictAndSave(train, test,paste('BORRAME','.csv'))
